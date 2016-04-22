a63cf3d1d21e241ea5401d429e1d6526177e7a52
Estefanía Morales Luis (alu0100698688) <alu0100698688@ull.edu.es>
2016-04-22 01:02:13 +0000

    Modified app.js for open connection with db. Two methods to search in db and one method to control the limit of entries in db

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express();
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/csv_db'); //Conexión a la base de datos
 
 
 
@@ -31,6 +33,53 @@ app.post('/csv', (request, response) => {
   response.render('csv', { "csv": calculate(csv), title:"Resultado" });
 });
 
+
+//Creación de un objeto EntradaCsv
+const Entrada = require('./models/modelo-db.js');
+
+//Eliminación de la última entrada para que siempre hayan 4 entradas en la bd
+app.get('/mongo', function(req, res) {
+    Entrada.find({}, function(err, entradas) {
+        if (err)
+            return err;
+        if (entradas.length >= 4) {
+            Entrada.find({ name: entradas[3].name }).remove().exec();
+        }
+    });
+    
+    //Creación de la entrada nueva a guardar
+    let input = new Entrada({
+        "name": req.query.name,
+        "content": req.query.content
+    });
+    
+    //Guardando la entrada nueva en la bd
+    input.save(function(err) {
+        if (err) {
+            console.log(`Error al guardar:\n${err}`);
+            return err;
+        }
+        console.log(`Guardado con éxito: ${input}`);
+    });
+});
+
+//Mostrar todas las entradas de la collection
+app.get('/find', function(req, res) {
+    Entrada.find({}, function(err, entradas) {
+        if (err)
+            return err;
+        res.send(entradas);
+    });
+});
+
+//Muestra el contenido asociado al nombre indicado
+app.get('/findName', function(req, res) {
+    Entrada.find({
+        name: req.query.name
+    }, function(err, contenido) {
+        res.send(contenido);
+    });
+});
 
 app.listen(app.get('port'), () => {
     console.log(`Node app is running at localhost: ${app.get('port')}` );
